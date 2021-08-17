b09a774a2efe82abbef8779f54a7a3b1a7810703
asimceman <aceman1@etf.unsa.ba>
2021-08-17 16:27:06 +0200

    First commit after loss

--- src/main/java/guru/springframework/spring5webapp/repositories/PublisherRepository.java
+++ src/main/java/guru/springframework/spring5webapp/repositories/PublisherRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.repository.CrudRepository;
  * Created by jt on 12/23/19.
  */
 public interface PublisherRepository extends CrudRepository<Publisher, Long> {
-    //
+    //as
 }
